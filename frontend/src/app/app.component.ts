e18a37b285ba7f7230b93cc80e469a20a7278531
Frank van der Linden <flinden68@elstarit.nl>
2018-08-27 16:44:22 +0200

    Added Topice Service for Frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {PreferenceService} from "./service/preferences.service";
 import {Preference} from "./model/preference";
+import {Topic} from "./model/topic";
+import {TopicService} from "./service/topics.service";
 
 @Component({
   selector: 'app-root',
@@ -11,8 +13,11 @@ export class AppComponent implements OnInit {
   title = 'Reactive Streams Demo';
 
   preferences : Preference[];
+  topics : Topic[];
 
-  constructor(private preferenceService: PreferenceService) { }
+  @Input() topic : Topic
+
+  constructor(private preferenceService: PreferenceService, private topicService : TopicService) { }
 
   ngOnInit() {
     //this.getPreferences();
@@ -26,5 +31,20 @@ export class AppComponent implements OnInit {
   deletePreference(preference: Preference):void {
     this.preferences = this.preferences.filter(h => h !== preference);
     this.preferenceService.deletePreference(preference.userid).subscribe();
+  }
+
+  loadTopics(){
+    this.topicService.getTopics()
+      .subscribe(topics => this.topics = topics);
+  }
+
+  createTopic(){
+    this.topicService.createTopic(this.topic)
+      .subscribe(topic => this.topics.push(topic));
+  }
+
+  deleteTopic(topic: Topic):void {
+    this.topics = this.topics.filter(h => h !== topic);
+    this.topicService.deleteTopic(topic.name).subscribe();
   }
 }
---
+++ frontend/src/app/service/topics.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Preference } from '../model/preference';
+import {Topic} from "../model/topic";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({ providedIn: 'root' })
+export class TopicService {
+
+  private serviceUrl = 'http://localhost:10000/reactive/streams';  // URL to web api
+
+  constructor(
+    private http: HttpClient) { }
+
+  public getTopicsAsStream() {
+    return this.http.get<Topic[]>(this.serviceUrl + '/topic/stream')
+      .pipe(
+        catchError(this.handleError('getTopicsAsStream', []))
+      );
+  }
+
+  public getTopics() {
+    return this.http.get<Topic[]>(this.serviceUrl + '/topic/all')
+      .pipe(
+        catchError(this.handleError('getTopics', []))
+      );
+  }
+
+  public getTopicByName(name:string) {
+    return this.http.get<Topic>(this.serviceUrl + '/topic/'+name)
+      .pipe(
+        catchError(this.handleError('getTopicByName', []))
+      );
+  }
+
+  public createTopic(topic:Topic): Observable<Topic> {
+    return this.http.post(this.serviceUrl + '/topic/create', topic, httpOptions)
+      .pipe(
+        tap((topic:Topic) => this.log(`added topic w/ id=${topic.id}`)),
+        catchError(this.handleError<Topic>('createTopic'))
+      );
+  }
+
+  public deleteTopic(name:string) {
+    return this.http.delete<String>(this.serviceUrl + '/topic/delete/'+name)
+      .pipe(
+        catchError(this.handleError('deleteTopic', []))
+      );
+  }
+
+  public deleteAllTopics() {
+    return this.http.delete<String>(this.serviceUrl + '/topic/delete/all')
+      .pipe(
+        catchError(this.handleError('deleteAllTopics', []))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    console.log(`PreferenceService: ${message}`);
+  }
+
+}
