463db2eb402581ce879db5b32b9360a4a146155f
Frank van der Linden <flinden68@elstarit.nl>
2018-10-03 07:57:05 +0200

    Add Suggestion Model Object

---
+++ src/main/java/nl/elstarit/reactive/model/Suggestion.java
@@ -0,0 +1,13 @@
+package nl.elstarit.reactive.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Suggestion extends BaseEntity {
+
+    private String name;
+}
